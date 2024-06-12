fde92a78b74ba89df4d72a1e7ff2d5b71ec33526
Roman Stas <romastas1996@gmail.com>
2024-06-12 23:12:09 +0300

    uniq and check

--- create_db.py
+++ create_db.py
@@ -17,33 +17,41 @@ def create_table(conn, create_table_sql):
 
 
 if __name__ == "__main__":
-    sql_create_projects_table = """
-    CREATE TABLE IF NOT EXISTS projects (
-     id integer PRIMARY KEY,
-     name text NOT NULL,
-     begin_date text,
-     end_date text
+    sql_create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+     id INTEGER PRIMARY KEY,
+     fullname VARCHAR(100) NOT NULL,
+     email VARCHAR(100) UNIQUE NOT NULL
+    );
+    """
+
+    sql_create_status_table = """
+    CREATE TABLE IF NOT EXISTS status (
+     id INTEGER PRIMARY KEY,
+     name VARCHAR(50) CHECK (name IN ('new', 'in progress', 'completed'))
     );
     """
 
     sql_create_tasks_table = """
     CREATE TABLE IF NOT EXISTS tasks (
-     id integer PRIMARY KEY,
-     name text NOT NULL,
-     priority integer,
-     project_id integer NOT NULL,
-     status Boolean default False,
-     begin_date text NOT NULL,
-     end_date text NOT NULL,
-     FOREIGN KEY (project_id) REFERENCES projects (id)
+     id INTEGER PRIMARY KEY,
+     title VARCHAR(100) NOT NULL,
+     description TEXT,
+     status_id INTEGER,
+     user_id INTEGER,
+     FOREIGN KEY (status_id) REFERENCES status (id)
+        ON DELETE SET NULL ON UPDATE CASCADE,
+     FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE SET NULL ON UPDATE CASCADE
     );
     """
 
     with create_connection(database) as conn:
         if conn is not None:
             # create projects table
-            create_table(conn, sql_create_projects_table)
-            # create tasks table
+            create_table(conn, sql_create_users_table)
+            create_table(conn, sql_create_status_table)
             create_table(conn, sql_create_tasks_table)
+
         else:
             print("Error! cannot create the database connection.")
