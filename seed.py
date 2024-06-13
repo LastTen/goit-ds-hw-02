536d486682ed761410bcb216f47a1b1c100b1185
Roman Stas <romastas1996@gmail.com>
2024-06-13 12:04:19 +0300

    refactor

--- seed.py
+++ seed.py
@@ -1,60 +1,28 @@
 from sqlite3 import Error
 from connect import create_connection, database
 
+sql_user = "INSERT INTO users(fullname,email) VALUES(?,?);"
+sql_status = "INSERT INTO status(name) VALUES(?);"
+sql_task = "INSERT INTO tasks(title,description,status_id,user_id) VALUES(?,?,?,?);"
 
-def create_user(conn, project):
-    sql = """
-    INSERT INTO users(fullname,email) VALUES(?,?);
-    """
+
+def insert_data(conn, sql, data):
     cur = conn.cursor()
     try:
-        cur.executemany(sql, project)
+        cur.executemany(sql, data)
         conn.commit()
     except Error as e:
         print(e)
     finally:
         cur.close()
 
-    return cur.lastrowid
-
-
-def create_status(conn, task):
-    sql = """
-    INSERT INTO status(name) VALUES(?);
-    """
-    cur = conn.cursor()
-    try:
-        cur.executemany(sql, task)
-        conn.commit()
-    except Error as e:
-        print(e)
-    finally:
-        cur.close()
-
-    return cur.lastrowid
-
-
-def create_task(conn, task):
-    sql = """
-    INSERT INTO tasks(title,description,status_id,user_id) VALUES(?,?,?,?);
-    """
-    cur = conn.cursor()
-    try:
-        cur.executemany(sql, task)
-        conn.commit()
-    except Error as e:
-        print(e)
-    finally:
-        cur.close()
-
-    return cur.lastrowid
+    return "create"
 
 
 if __name__ == "__main__":
     with create_connection(database) as conn:
         # Statuses
         status = [("new",), ("in progress",), ("completed",)]
-        create_status(conn, status)
         # Users
         users = [
             ("John Smit", "smith@gmail.com"),
@@ -69,8 +37,9 @@ if __name__ == "__main__":
             ("test4", "some desc", 3, 1),
             ("test5", "some desc", 1, 2),
         )
-        create_status(conn, status)
-        create_user(conn, users)
-        create_task(conn, task)
+
+        insert_data(conn, sql_status, status)
+        insert_data(conn, sql_user, users)
+        insert_data(conn, sql_task, task)
 
         print("all complete")
