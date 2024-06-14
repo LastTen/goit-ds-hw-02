e6669f15779ce6925b5bf667416abb2dbd11ebdd
Roman Stas <romastas1996@gmail.com>
2024-06-14 12:38:15 +0300

    select

--- query_to_db.py
+++ query_to_db.py
@@ -1,96 +1,134 @@
-from sqlite3 import Error
-from src.connect import create_connection, database
 from src.modules.sql_query import execute_query
 
 
-sql = """
-    SELECT title, description, s.name as status FROM tasks as t
-    join status s on s.id = t.status_id
-    WHERE t.user_id = ?
-    """
+def get_all_user_task(id):
+    sql = """
+        SELECT title, description, s.name as status FROM tasks as t
+        join status s on s.id = t.status_id
+        WHERE t.user_id = ?
+        """
+
+    return execute_query(sql, [str(id)])
 
 
-sql = """
-    SELECT title, description FROM tasks as t
-    join status s on s.id = t.status_id
-    WHERE s.name = ?
-    """
+def get_all_task_by_status(status):
+    sql = """
+        SELECT title, description FROM tasks as t
+        join status s on s.id = t.status_id
+        WHERE s.name = ?
+        """
+    return execute_query(sql, [status])
 
 
-sql = """
-    UPDATE tasks as t
-    SET status_id = (SELECT s.id FROM status as s WHERE s.name = ?)
-    WHERE t.id = ?
-    """
+# def update_task_status(status, task_id):
+#     sql = """
+#         UPDATE tasks as t
+#         SET status_id = (SELECT s.id FROM status as s WHERE s.name = ?)
+#         WHERE t.id = ?
+#         """
+#     execute_query(sql, [status, str(task_id)])
+#     return f"status task with id {task_id} updated to {status}"
 
 
-sql = """
-    INSERT INTO tasks(title, description, status_id, user_id)
-    VALUES(?, ?, 1, ?)
-    """
+# sql = """
+#     INSERT INTO tasks(title, description, status_id, user_id)
+#     VALUES(?, ?, 1, ?)
+#     """
 
 
-sql = """
-    SELECT u.fullname, u.email FROM users as u 
-    WHERE u.id NOT IN (SELECT t.user_id FROM tasks as t)
-    """
+def get_user_wo_task():
+    sql = """
+        SELECT u.fullname, u.email FROM users as u
+        WHERE u.id NOT IN (SELECT t.user_id FROM tasks as t)
+        """
+    return execute_query(sql)
 
 
-sql = """
-    SELECT t.title, t.description FROM tasks t
-    where t.status_id IS NOT 3
-    """
+def get_no_complete_task():
+    sql = """
+        SELECT t.title, t.description FROM tasks t
+        where t.status_id IS NOT 3
+        """
+    return execute_query(sql)
 
 
-sql = """
-    DELETE FROM tasks 
-    where id = ?
-    """
+# sql = """
+#     DELETE FROM tasks
+#     where id = ?
+#     """
 
 
-sql = """
-    SELECT u.fullname, u.email FROM users u 
-    WHERE u.email LIKE ?
-    """
+def get_user_with_email(email):
+    sql = """
+        SELECT u.fullname, u.email FROM users u
+        WHERE u.email LIKE ?
+        """
+    return execute_query(sql, [f"%{email}%"])
 
 
-sql = """
-    UPDATE users 
-    SET fullname = ? 
-    WHERE id = ?
-"""
+# def update_user_name(new_fullname, id):
+#     sql = """
+#         UPDATE users
+#         SET fullname = ?
+#         WHERE id = ?
+#     """
+#     execute_query(sql, [f"'{new_fullname}'", str(id)])
 
 
-sql = """
-    SELECT s.name as status, COUNT(t.id) as count_task FROM tasks as t
-    JOIN status as s ON s.id = t.status_id 
-    GROUP BY t.status_id 
-    """
+def get_count_task_each_status():
+    sql = """
+        SELECT s.name as status, COUNT(t.id) as count_task FROM tasks as t
+        JOIN status as s ON s.id = t.status_id
+        GROUP BY t.status_id
+        """
+    return execute_query(sql)
 
 
-sql = """
-    SELECT title, description, s.name as status FROM tasks as t
-    JOIN status s on s.id = t.status_id
-    JOIN users u on u.id = t.user_id 
-    WHERE u.email LIKE ?
-    """
+def get_tasks_by_user_email_domain(domain):
+    sql = """
+        SELECT title, description, s.name as status FROM tasks as t
+        JOIN status s on s.id = t.status_id
+        JOIN users u on u.id = t.user_id
+        WHERE u.email LIKE ?
+        """
+    return execute_query(sql, [f"%@{domain}"])
 
 
-sql = """
-    SELECT t.title FROM tasks t
-    WHERE t.description ISNULL 
-    """
+def get_tasks_wo_description():
+    sql = """
+        SELECT t.title FROM tasks t
+        WHERE t.description ISNULL
+        """
+    return execute_query(sql)
 
 
-sql = """
-    SELECT u.fullname, t.title, t.description FROM users u
-    JOIN tasks t ON t.user_id = u.id 
-    JOIN status s ON s.id = t.status_id AND s.name = 'in progress'
-    """
+def get_tasks_in_progress():
+    sql = """
+        SELECT u.fullname, t.title, t.description FROM users u
+        JOIN tasks t ON t.user_id = u.id
+        JOIN status s ON s.id = t.status_id AND s.name = 'in progress'
+        """
+    return execute_query(sql)
 
 
-sql = """
-    SELECT u.fullname as user, COUNT(t.id) as count_task FROM users as u
-    LEFT JOIN tasks t ON t.user_id = u.id
-    GROUP BY u.id 
-    """
+def get_count_task_for_each_user():
+    sql = """
+        SELECT u.fullname as user, COUNT(t.id) as count_task FROM users as u
+        LEFT JOIN tasks t ON t.user_id = u.id
+        GROUP BY u.id
+        """
+    return execute_query(sql)
+
+
+if __name__ == "__main__":
+    # print(get_all_user_task(1))
+    # print(get_all_task_by_status("new"))
+    # print(update_task_status("new", 2))
+    # print(get_user_wo_task())
+    # print(get_no_complete_task())
+    # print(get_user_with_email("77"))
+    # print(get_count_task_each_status())
+    # print(get_tasks_by_user_email_domain("example.net"))
+    # print(get_tasks_wo_description())
+    # print(get_tasks_in_progress())
+    print(get_count_task_for_each_user())
